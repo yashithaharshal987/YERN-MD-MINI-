761434e2692be9b25a219d1be1184951fc68e076
Yern <yashithaharshal05@gmail.com>
2026-04-08 16:41:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l06cb8.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM YERN MD CREATED BY YERN <NOW ALIVE> "
+};
